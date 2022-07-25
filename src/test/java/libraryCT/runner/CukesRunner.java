f50ffbc70c7da35b9a8ae2fea9d86cc77cbb3caf
Aleksey <alecvelitskiy@gmail.com>
2022-07-25 01:41:36 -0400

    Alexey. LibraryCT

--- src/test/java/libraryCT/runner/CukesRunner.java
+++ src/test/java/libraryCT/runner/CukesRunner.java
@@ -17,7 +17,7 @@ import org.junit.runner.RunWith;
             features = "src/test/resources/features",
             glue = "libraryCT/stepDefinitions",
             dryRun = false,
-            tags = ""
+            tags = " @addingANewUser"
 
 
     )
---
+++ src/test/java/libraryCT/stepDefinitions/AddNewUserStepDef.java
@@ -0,0 +1,58 @@
+package libraryCT.stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import libraryCT.pages.AddNewUserPage;
+import libraryCT.pages.LoginPage;
+import libraryCT.utilities.BrowserUtilities;
+import libraryCT.utilities.ConfigurationReader;
+import libraryCT.utilities.Driver;
+
+
+public class AddNewUserStepDef {
+
+    AddNewUserPage addNewUserPage = new AddNewUserPage();
+    LoginPage loginPage = new LoginPage();
+
+    @Given("librarian is on the homePage")
+    public void librarian_is_on_the_home_page() {
+        Driver.getDriver().get(ConfigurationReader.getProperty("env"));
+        loginPage.login(ConfigurationReader.getProperty("librarian"), ConfigurationReader.getProperty("librarianPassword"));
+        BrowserUtilities.waitFor(1);
+    }
+
+    @When("librarian clicks Books module")
+    public void librarian_clicks_books_module() {
+        addNewUserPage.booksModuleClick();
+        BrowserUtilities.waitFor(1);
+    }
+
+    @When("librarian clicks {string} button")
+    public void librarian_clicks_button(String string) {
+        addNewUserPage.bookBtnClick();
+        BrowserUtilities.waitFor(1);
+    }
+
+    @When("librarian enter BookName, ISBN, Year, Author and Description")
+    public void librarian_enter_book_name_isbn_year_author_and_description() {
+        addNewUserPage.inputBookName();
+        addNewUserPage.inputISBN();
+        addNewUserPage.inputYear();
+        addNewUserPage.inputAuthor();
+        addNewUserPage.inputDescription();
+        BrowserUtilities.waitFor(1);
+    }
+
+    @When("librarian clicks save changes button")
+    public void librarian_clicks_save_changes_button() {
+        addNewUserPage.clickSaveChangesBtn();
+        BrowserUtilities.waitFor(1);
+    }
+
+    @Then("verify anew book is added")
+    public void verify_anew_book_is_added() {
+        addNewUserPage.confirmationMsgIsDisplayed();
+    }
+}
+
